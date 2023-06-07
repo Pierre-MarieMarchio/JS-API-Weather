c094461eef82dd54fe30d1ee612db18a088820fd
Pierre-Marie <pierremariemarchio@gmail.com>
2023-06-07 17:42:56 +0200

    hourly forcast

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,16 @@
 let apiResults;
 
+//  const for curent weather
+
+const weather = document.querySelector(".weather");
+const temp = document.querySelector(".temp");
+const location = document.querySelector(".location");
+
+// const for curent weather hourly forecast predictions
+
+const hourlyForecast = document.querySelectorAll(".hourly-forecast");
+const hourlyTemp = document.querySelectorAll(".hourly-temp");
+
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
     (position) => {
@@ -28,5 +39,23 @@ function APICall(long, lat) {
     .then((data) => {
       apiResults = data;
       console.log(apiResults);
+
+      weather.innerText = apiResults.list[0].weather[0].main;
+      temp.innerText = `${Math.round(apiResults.list[0].main.temp)}°C`;
+      location.innerText = apiResults.city.name;
+
+      //   hourly predictions
+
+      for (let i = 0; i < hourlyForecast.length; i++) {
+        let forecastHour1 = apiResults.list[i].dt_txt.split("")[11];
+        let forecastHour2 = apiResults.list[i].dt_txt.split("")[12];
+        hourlyForecast[i].innerText =
+          Math.round(forecastHour1 + forecastHour2) + " h";
+      }
+
+      for (let j = 0; j < hourlyTemp.length; j++) {
+        hourlyTemp[j].innerText =
+          Math.round(apiResults.list[j].main.temp) + "°C";
+      }
     });
 }
